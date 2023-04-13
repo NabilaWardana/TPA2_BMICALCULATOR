16945ee8cb1f6e18f476763d99ce59e180b2221c
NabilaWardana <20081010114@student.upnjatim.ac.id>
2023-04-13 14:27:25 +0700

    add js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//fungsi untuk mengecek angka yang diinputkan tidak kurang dari = 0
+function checkNumber(num){
+    return num<=0 ? false : true
+}
+
 function checkBmi(){
-    
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
+    if(checkNumber(weight)&& checkNumber(height)){
+        const bmi = (weight/Math.pow(height/100,2)).toFixed(1);
+        let status;
+        if(bmi >= 30){
+            status = "Obesity"
+        }
+        else if(bmi >= 25 && bmi <30){
+            status = "Overweight"
+        }
+        else if(bmi >= 18.5 && bmi <25){
+            status = "Normal Weight"
+        }
+        else{
+            status = "Underweight"
+        }
+        document.getElementById("hasil").innerHTML = "Your BMI is <b>"+bmi+"</b> which means You are <b>"+status+"</b>";
+    }
 }
